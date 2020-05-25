4edff9b12c378e85643992811befa691cdcbe9c4
linux <linux@linux-virtual-machine>
2020-05-25 19:07:26 +0530

    Test2 Committed to BranchA

---
+++ src/GitDemo/Test2.java
@@ -0,0 +1,16 @@
+package GitDemo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "yes";
+
+		if (hungry == "yes" || hungry == "yes") {
+			System.out.println("Eat Something......");
+		} else {
+			System.out.println("Do Work...........");
+		}
+	}
+}
